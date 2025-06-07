36efcf94d087f0d3e9b7b8cbe4c5e7a92b828959
Tom Mwambingu <[tomchom30@gmail.com]>
2025-06-07 10:09:05 +0300

    Add Slideshow

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('orderForm').addEventListener('submit', function (e) {
   const location = document.getElementById('location').value;
   const quantity = document.getElementById('quantity').value;
 
-  const message = `Hello Steaze Punch 🍹,%0AMy name is *${name}* and I’d like to order *${quantity}* bottle(s).%0A📍 Delivery Location: ${location}%0A📞 Contact: ${phone}`;
+  const message = `Hello Steaze Punch 🍹,%0AMy name is *${name}* and I’d like to order *${quantity}* SP(s).%0A📍 Delivery Location: ${location}%0A📞 Contact: ${phone}`;
 
   const whatsappNumber = '254706867627'; 
   const url = `https://wa.me/${whatsappNumber}?text=${message}`;
